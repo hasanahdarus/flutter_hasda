865a5c2d3517e53ea74180cc7583af9b612b00a8
Hasda12 <143325859+Hasda12@users.noreply.github.com>
2023-11-18 12:13:08 +0700

    pertemuan 3

---
+++ 02_branch_loop_function/branch.dart
@@ -0,0 +1,25 @@
+void main(){
+  var usia = 18;
+  print(usia > 20);
+
+  if (usia < 20) {
+    print("Dewasa");
+  } else {
+    print("Anda tidak memiliki golongan");
+  }
+
+  print("sesuatu");
+  print("--------------------------------------");
+
+  var umur = 40;
+  print(umur > 20);
+
+  if (umur < 20) {
+    print("Remaja");
+  } else if (umur < 50) {
+    print("Dewasa");
+  }else {
+    print("Anda tidak memiliki golongan");
+  }
+  print("sesuatu");
+}
